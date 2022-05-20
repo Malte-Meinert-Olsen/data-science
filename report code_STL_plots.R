73e6c4727dc7ea2956c2609062a4b75983509d45
Malte-Meinert-Olsen <malte.olsen@hotmail.com>
2022-05-20 17:10:00 +0200

    Cleaning the STL code and making the code for plotting flow to investigate response time

--- report code_STL_plots.R
+++ report code_STL_plots.R
@@ -8,37 +8,46 @@ source("report_code_setup.R")
 
 #Making the decomposition
 
-
+#Removing the time scale and replacing with observation
 flow <- data_hour %>%
   mutate(t = row_number()) %>%
   update_tsibble(index = t, regular = TRUE)
 
+#Plotting the STL decomposition with all season being periodic
 flow %>%
   model(
-    STL(flow_effluent_m3_h ~ season(period = 24) +
-          season(period = 7*24)+
-          season(period = 365.25*24),
+    STL(flow_effluent_m3_h ~ season(period = 24, 
+                                    window="periodic") +
+          season(period = 7*24, 
+                 window="periodic")+
+          season(period = 365.25*24, 
+                 window="periodic"),
         robust = TRUE)) %>%
   components() %>%
-  rename("Orginal timeseries"=flow_effluent_m3_h,
-         "Trend component"=trend,
-         "Daily seasonality"=season_24,
-         "Weekly seasonality"=season_168,
-         "Yealy seasonality"=season_8766,
-         "Remainder component"=remainder) %>% 
-  ggplot(gather(dat, component, value, -Time), aes(Time, value)) +
-  facet_grid(component ~ ., scales="free_y") +
-  geom_line()+ labs(x = "Observation")
-
-
-
-STL_comp <- flow %>%
-  model(
-    STL(flow_effluent_m3_h ~ season(period = 24) +
-          season(period = 7*24)+
-          season(period = 365.25*24),
-        robust = TRUE)) %>%
-  components() %>% 
-  autoplot()+
+  autoplot() +
   labs(x = "Hours after 2018-01-01 00:00:00")
+
+#Plotting STL decomposition with windows of 93, 51 and no yearly seasonality
+flow %>%
+  model(
+    STL(flow_effluent_m3_h ~ season(period = 24, 
+                                    window=93) +
+          season(period = 7*24, 
+                 window=51),
+        robust = TRUE)) %>%
+  components() %>%
+  autoplot() +
+  labs(x = "Hours after 2018-01-01 00:00:00")
+
+#Plotting STL decomposition with windows of 93, and no weekly and yearly seasonality
+flow %>%
+  model(
+    STL(flow_effluent_m3_h ~ season(period = 24, 
+                                    window=93),
+        robust = TRUE)) %>%
+  components() %>%
+  autoplot() +
+  labs(x = "Hours after 2018-01-01 00:00:00")
+
+
 
---
+++ report_code_plots_to_find_responstime_at_rain_events.R
@@ -0,0 +1,262 @@
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#Plots to find time in sewers and to ammonium and nitrate peak
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+#Setup 
+
+source("report_code_setup.R")
+
+#Overview of the rain fall
+
+data_hour %>% autoplot(rainfall_mm)
+
+#To overview the data and find suitable periods
+
+gridExtra::grid.arrange(data_hour %>% 
+                          filter_index("2018") %>% 
+                          autoplot(rainfall_mm),
+                        data_hour %>% 
+                          filter_index("2019") %>% 
+                          autoplot(rainfall_mm),
+                        data_hour %>% 
+                          filter_index("2020") %>% 
+                          autoplot(rainfall_mm),
+                        data_hour %>% 
+                          filter_index("2021") %>% 
+                          autoplot(rainfall_mm),
+                        data_hour %>% 
+                          filter_index("2022") %>% 
+                          autoplot(rainfall_mm)
+)
+
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#Rain in 2018
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+#To find days of interrest
+gridExtra::grid.arrange(data_hour %>% 
+                          filter_index("2018-07") %>% 
+                          autoplot(rainfall_mm),
+                        data_hour %>% 
+                          filter_index("2018-08") %>% 
+                          autoplot(rainfall_mm),
+                        data_hour %>% 
+                          filter_index("2018-09") %>% 
+                          autoplot(rainfall_mm)
+)
+
+
+#Zooming in on the day
+data_hour %>% 
+  filter_index("2018-07"~"2018-09") %>% 
+  autoplot(rainfall_mm)
+
+
+#28th august 2018
+gridExtra::grid.arrange(data_one_min %>% 
+                          filter_index("2018-07-28 20:00"~"2018-07-29 00:00") %>% 
+                          autoplot(rainfall_mm),
+                        data_one_min %>% 
+                          filter_index("2018-07-28 20:00"~"2018-07-29 00:00") %>%
+                          autoplot(flow_effluent_m3_h),
+                        data_one_min %>% 
+                          filter_index("2018-07-28 20:00"~"2018-07-29 20:00") %>%
+                          ggplot(aes(time_one_min)) + 
+                          geom_line(aes(y = ammonium_effluent_mg_L)) +
+                          geom_line(aes(y = nitrate_effluent_mg_L, 
+                                        col="Red", 
+                                        alpha=0.6))+
+                          geom_hline(yintercept = 2, 
+                                     col="Blue")+
+                          theme(legend.position="none")
+)
+
+#Extra zoom on the 28th of august
+gridExtra::grid.arrange(data_one_min %>% 
+                          filter_index("2018-07-28 20:00"~"2018-07-28 21:00") %>% 
+                          autoplot(rainfall_mm),
+                        data_one_min %>% 
+                          filter_index("2018-07-28 20:00"~"2018-07-28 22:00") %>%
+                          autoplot(flow_effluent_m3_h),
+                        data_fifteen_min %>% 
+                          filter_index("2018-07-28 20:00"~"2018-07-29 20:00") %>%
+                          ggplot(aes(time_fifteen_min)) + 
+                          geom_line(aes(y = ammonium_effluent_mg_L)) +
+                          geom_line(aes(y = nitrate_effluent_mg_L, 
+                                        col="Red", 
+                                        alpha=0.6))+
+                          geom_hline(yintercept = 2, 
+                                     col="Blue")+
+                          theme(legend.position="none")
+)
+
+
+
+#9th of september 2018
+
+data_hour %>% 
+  filter_index("2018-09-07") %>% 
+  autoplot(rainfall_mm)
+
+
+gridExtra::grid.arrange(data_one_min %>% 
+                          filter_index("2018-09-07") %>% 
+                          autoplot(rainfall_mm),
+                        data_one_min %>% 
+                          filter_index("2018-09-07") %>%
+                          autoplot(flow_effluent_m3_h),
+                        data_one_min %>% 
+                          filter_index("2018-09-07") %>%
+                          ggplot(aes(time_one_min)) + 
+                          geom_line(aes(y =ammonium_effluent_mg_L)) +
+                          geom_line(aes(y = nitrate_effluent_mg_L, 
+                                        col="Red", 
+                                        alpha=0.6))+
+                          geom_hline(yintercept = 2, 
+                                     col="Blue")+
+                          theme(legend.position="none")
+)
+
+#Extra zoom
+gridExtra::grid.arrange(data_one_min %>% 
+                          filter_index("2018-09-07 10:00"~"2018-09-07 14:00") %>% 
+                          autoplot(rainfall_mm),
+                        data_one_min %>% 
+                          filter_index("2018-09-07 10:00"~"2018-09-07 14:00") %>%
+                          autoplot(flow_effluent_m3_h),
+                        data_fifteen_min %>% 
+                          filter_index("2018-09-07 10:00"~"2018-09-07 23:00") %>%
+                          ggplot(aes(time_fifteen_min)) + 
+                          geom_line(aes(y =ammonium_effluent_mg_L)) +
+                          geom_line(aes(y = nitrate_effluent_mg_L, 
+                                        col="Red", 
+                                        alpha=0.6))+
+                          geom_hline(yintercept = 2, 
+                                     col="Blue")+
+                          theme(legend.position="none")
+)
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#Rain in 2019
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
+#2019-08-27
+data_hour %>% 
+  filter_index("2019-08-27"~"2019-08-28") %>% 
+  autoplot(rainfall_mm)
+
+#Extra zoom
+gridExtra::grid.arrange(data_one_min %>% 
+                          filter_index("2019-08-27 17:00"~"2019-08-27 19:00") %>% 
+                          autoplot(rainfall_mm),
+                        data_one_min %>% 
+                          filter_index("2019-08-27 17:00"~"2019-08-27 19:00") %>%
+                          autoplot(flow_effluent_m3_h),
+                        data_fifteen_min %>% 
+                          filter_index("2019-08-27 13:00"~"2019-08-28 04:00") %>%
+                          ggplot(aes(time_fifteen_min)) + 
+                          geom_line(aes(y =ammonium_effluent_mg_L)) +
+                          geom_line(aes(y = nitrate_effluent_mg_L, 
+                                        col="Red", 
+                                        alpha=0.6))+
+                          geom_hline(yintercept = 2, 
+                                     col="Blue")+
+                          theme(legend.position="none")
+)
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#Rain in 2020
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+#19th June 2020
+data_hour %>% 
+  filter_index("2020-06-19") %>% 
+  autoplot(rainfall_mm)
+
+
+gridExtra::grid.arrange(data_five_min %>% 
+                          filter_index("2020-06-19 11:00"~"2020-06-19 14:00") %>% 
+                          autoplot(rainfall_mm),
+                        data_five_min %>% 
+                          filter_index("2020-06-19 11:00"~"2020-06-19 14:00") %>%
+                          autoplot(flow_effluent_m3_h),
+                        data_fifteen_min %>% 
+                          filter_index("2020-06-19 08:00"~"2020-06-19 23:00") %>%
+                          ggplot(aes(time_fifteen_min)) + 
+                          geom_line(aes(y =ammonium_effluent_mg_L)) +
+                          geom_line(aes(y = nitrate_effluent_mg_L, 
+                                        col="Red", 
+                                        alpha=0.6))+
+                          geom_hline(yintercept = 2, 
+                                     col="Blue")+
+                          theme(legend.position="none")
+)
+
+
+
+#18th of august 2020
+data_hour %>% 
+  filter_index("2020-08-18") %>% 
+  autoplot(rainfall_mm)
+
+#Extra zoom
+gridExtra::grid.arrange(data_one_min %>% 
+                          filter_index("2020-08-18 11:00"~"2020-08-18 14:00") %>% 
+                          autoplot(rainfall_mm),
+                        data_one_min %>% 
+                          filter_index("2020-08-18 11:00"~"2020-08-18 14:00") %>%
+                          autoplot(flow_effluent_m3_h),
+                        data_fifteen_min %>% 
+                          filter_index("2020-08-18 11:00"~"2020-08-18 23:00") %>%
+                          ggplot(aes(time_fifteen_min)) + 
+                          geom_line(aes(y =ammonium_effluent_mg_L)) +
+                          geom_line(aes(y = nitrate_effluent_mg_L, 
+                                        col="Red", 
+                                        alpha=0.6))+
+                          geom_hline(yintercept = 2, 
+                                     col="Blue")+
+                          theme(legend.position="none")
+)
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#Rain in 2021
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+#20th of June 2021
+data_hour %>% 
+  filter_index("2021-06-20") %>% 
+  autoplot(rainfall_mm)
+
+#Extra zoom
+gridExtra::grid.arrange(data_five_min %>% 
+                          filter_index("2021-06-20 10:00"~"2021-06-20 13:00") %>% 
+                          autoplot(rainfall_mm),
+                        data_five_min %>% 
+                          filter_index("2021-06-20 10:00"~"2021-06-20 13:00") %>%
+                          autoplot(flow_effluent_m3_h),
+                        data_fifteen_min %>% 
+                          filter_index("2021-06-20 10:00"~"2021-06-21 12:00") %>%
+                          ggplot(aes(time_fifteen_min)) + 
+                          geom_line(aes(y =ammonium_effluent_mg_L)) +
+                          geom_line(aes(y = nitrate_effluent_mg_L, 
+                                        col="Red", 
+                                        alpha=0.6))+
+                          geom_hline(yintercept = 2, 
+                                     col="Blue")+
+                          theme(legend.position="none")
+)
